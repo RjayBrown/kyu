3c157a543686783a90c0e40db448e8d463833bf2
RjayBrown <rjbrown.dev@outlook.com>
2024-07-30 19:44:17 -0400

    day 6

--- kyu.js
+++ kyu.js
@@ -78,3 +78,15 @@ function filter_list(l) {
     // Return a new array with the strings filtered out
     return l.filter(v => typeof v !== 'string')
 }
+
+/*
+Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+Examples input/output:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false
+*/
