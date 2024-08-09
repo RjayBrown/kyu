389715ad85c0446b8e6a2f5565e4766140643da8
RjayBrown <rjbrown.dev@outlook.com>
2024-08-09 16:31:09 -0400

    added tribonacciSeq kata

--- 6kyu/likes.js
+++ 6kyu/likes.js
@@ -1,3 +1,5 @@
+/* Day 20 */
+
 /*
 You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
 
