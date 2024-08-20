21514a982cdab4efbd33c2c007b3570f4b23ae94
RjayBrown <rjbrown.dev@outlook.com>
2024-08-19 23:27:04 -0400

    added checkUpperCase kata

---
+++ 8kyu/checkUpperCase.js
@@ -0,0 +1,16 @@
+/* Day 28 */
+
+/*
+"c" -> False
+"C" -> True
+"hello I AM DONALD" -> False
+"HELLO I AM DONALD" -> True
+"ACSKLDFJSgSKLDFJSKLDFJ" -> False
+"ACSKLDFJSGSKLDFJSKLDFJ" -> True
+In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
+*/
+
+String.prototype.isUpperCase = function () {
+  // your code here
+  return this.split('').every(ltr => ltr === ltr.toUpperCase())
+}
