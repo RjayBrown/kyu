2d64bbcfe38586b87dba704dc0566cc8c1260831
RjayBrown <rjbrown.dev@outlook.com>
2024-10-05 23:53:59 -0400

    added insert method for tree

--- 8kyu/dsakyu/tree.js
+++ 8kyu/dsakyu/tree.js
@@ -8,6 +8,42 @@ class Node {
 
 class Tree {
     constructor(value) {
-        this.root = new Node(value)
+        if (value) {
+            this.root = new Node(value)
+        } else {
+            this.root = null
+        }
+    }
+
+    insert(value) {
+        const newNode = new Node(value)
+        if (this.root === null) {
+            this.root = newNode
+            return this
+        } else {
+            let current = this.root
+
+            while (true) {
+                if (newNode.value === current.value) { // Checks for duplicate value
+                    return undefined
+                }
+
+                if (newNode.value < current.value) { // condition for left insert
+                    if (current.left === null) {
+                        current.left = newNode
+                        return this
+                    } else {
+                        current = current.left
+                    }
+                } else { // condition for right insert - newNode.value > current.value
+                    if (current.right === null) {
+                        current.right = newNode
+                        return this
+                    } else {
+                        current = current.right
+                    }
+                }
+            }
+        }
     }
 }
