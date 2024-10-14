63bba4d00887501cc64fb0ef68d160c978a874cc
RjayBrown <rjbrown.dev@outlook.com>
2024-10-13 22:03:18 -0400

    added removeExclamationMarks kata

---
+++ 8kyu/removeExclamationMarks.js
@@ -0,0 +1,12 @@
+/* Day 61 */
+
+/*
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+*/
+
+function removeExclamationMarks(s) {
+    return s
+        .split('')
+        .filter(char => char !== '!')
+        .join('');
+}
